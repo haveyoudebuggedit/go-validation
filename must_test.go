dcf4543bb65e38405961e6ff009c1f6540d87696
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-03-23 12:31:23 +0100

    Changed tests

--- must_test.go
+++ must_test.go
@@ -1,39 +1,39 @@
 package validation_test
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/haveyoudebuggedit/validation"
+    "go.debugged.it/validation"
 )
 
 func ExampleMust() {
-	createWithError := func() (string, error) {
-		return "Hello world!", nil
-	}
+    createWithError := func() (string, error) {
+        return "Hello world!", nil
+    }
 
-	fmt.Println(validation.Must(createWithError()))
+    fmt.Println(validation.Must(createWithError()))
 
-	// Output: Hello world!
+    // Output: Hello world!
 }
 
 func ExampleMust3() {
-	createWithError := func() (string, string, error) {
-		return "Hello ", "world!", nil
-	}
+    createWithError := func() (string, string, error) {
+        return "Hello ", "world!", nil
+    }
 
-	value1, value2 := validation.Must3(createWithError())
-	fmt.Printf("%s%s", value1, value2)
+    value1, value2 := validation.Must3(createWithError())
+    fmt.Printf("%s%s", value1, value2)
 
-	// Output: Hello world!
+    // Output: Hello world!
 }
 
 func ExampleMust4() {
-	createWithError := func() (string, string, string, error) {
-		return "Hello ", "world", "!", nil
-	}
+    createWithError := func() (string, string, string, error) {
+        return "Hello ", "world", "!", nil
+    }
 
-	value1, value2, value3 := validation.Must4(createWithError())
-	fmt.Printf("%s%s%s", value1, value2, value3)
+    value1, value2, value3 := validation.Must4(createWithError())
+    fmt.Printf("%s%s%s", value1, value2, value3)
 
-	// Output: Hello world!
+    // Output: Hello world!
 }

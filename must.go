174858e886d616e26fe3b548dd9822a4a43c6702
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-03-23 12:30:53 +0100

    gofmt

--- must.go
+++ must.go
@@ -7,10 +7,10 @@ package validation
 //
 //     result := validation.Must(functionThatMayReturnAnError())
 func Must[K any](value K, err error) K {
-    if err != nil {
-        panic(err)
-    }
-    return value
+	if err != nil {
+		panic(err)
+	}
+	return value
 }
 
 // Must3 takes three arguments: two values and an error. If the error is not nil, it panics with the error, otherwise it
@@ -20,10 +20,10 @@ func Must[K any](value K, err error) K {
 //
 //     result1, result2 := validation.Must3(functionThatMayReturnAnError())
 func Must3[K any, L any](value1 K, value2 L, err error) (K, L) {
-    if err != nil {
-        panic(err)
-    }
-    return value1, value2
+	if err != nil {
+		panic(err)
+	}
+	return value1, value2
 }
 
 // Must4 takes four arguments: three values and an error. If the error is not nil, it panics with the error, otherwise
@@ -33,8 +33,8 @@ func Must3[K any, L any](value1 K, value2 L, err error) (K, L) {
 //
 //     result1, result2, result3 := validation.Must4(functionThatMayReturnAnError())
 func Must4[K any, L any, M any](value1 K, value2 L, value3 M, err error) (K, L, M) {
-    if err != nil {
-        panic(err)
-    }
-    return value1, value2, value3
+	if err != nil {
+		panic(err)
+	}
+	return value1, value2, value3
 }
